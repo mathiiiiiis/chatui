5fd7133b4fe18e052aa65c8c5b207afc84c62c59
mathiiiiiis <mathis@mathiiis.de>
2026-04-14 09:21:56 +0200

    feat(leave): leave rooms and delete if last user

--- backend/app/src/main/java/ClientHandler.java
+++ backend/app/src/main/java/ClientHandler.java
@@ -65,31 +65,52 @@ public class ClientHandler implements Runnable {
         sender.flush();
     }
 
+    public void leaveOrDeleteRoom() throws IOException {
+        String responseText = null;
+        String oldRoom = currentRoom;
+
+        if (currentRoom != null) {
+            rooms.get(currentRoom).getClients().remove(this);
+            responseText = "You left ";
+            if (rooms.get(currentRoom).getClients().isEmpty()) {
+                rooms.remove(currentRoom);
+                System.out.println("Deleted " + oldRoom + ", because empty");
+            }
+            currentRoom = null;
+        }
+
+        System.out.println(username + " left " + oldRoom);
+        sender.write(gson.toJson(new MessageModel("leave", responseText + oldRoom, null)));
+        sender.newLine();
+        sender.flush();
+    }
+
     public void streamReadMessages() throws IOException {
         String message;
         while ((message = reader.readLine()) != null) {
             MessageModel msg = gson.fromJson(message, MessageModel.class);
             MessageModel outgoing = new MessageModel(msg.getType(), msg.getContent(), username);
-            if (msg.getType().equals("room.join")) {
-                joinOrCreateRoom(msg.getContent());
-            } else {                
-                if (currentRoom == null) {
-                    sender.write(gson.toJson(new ErrorModel("error.room", "No Room joined", "Please join a room before sending a message.", 2)));
-                    sender.newLine();
-                    sender.flush();
-                    continue;
-                }
-                if (msg.getType().equals("room.message")) {
-                    for (ClientHandler c : rooms.get(currentRoom).getClients()) {
-                        if (c == this) {
-                            continue;
+            switch (msg.getType()) {
+                case "room.join" -> joinOrCreateRoom(msg.getContent());
+                case "room.leave" -> leaveOrDeleteRoom();
+                case "room.message" -> {
+                        if (currentRoom == null) {
+                            sender.write(gson.toJson(new ErrorModel("error.room", "No Room joined", "Please join a room before sending a message.", 2)));
+                            sender.newLine();
+                            sender.flush();
+                            break;
+                        } 
+                        for (ClientHandler c : rooms.get(currentRoom).getClients()) {
+                            if (c == this) {
+                                continue;
+                            }
+                            c.sender.write(gson.toJson(outgoing));
+                            System.out.println("New message: " + gson.toJson(outgoing));
+                            c.sender.newLine();
+                            c.sender.flush();
                         }
-                        c.sender.write(gson.toJson(outgoing));
-                        System.out.println("New message: " + gson.toJson(outgoing));
-                        c.sender.newLine();
-                        c.sender.flush();
-                    }
-                } else {
+                }
+                default -> { 
                     sender.write(gson.toJson(new ErrorModel("error.message", "Error while sending message", "Failed to send message to room: " + currentRoom + ", does it still exist?", 2)));
                     System.out.println(username + "'s message failed to send to room: " + currentRoom);
                     sender.newLine();
